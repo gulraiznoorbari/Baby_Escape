9ba01eb99767322670624d65f64188502cd5957a
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-18 10:33:13 +0500

    📦 NEW: Add Character Movement + Rotation

--- Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Touch _touch;
-    private float _speed = 0.5f;
+    private float _speed = 10f;
+    private float _rotationSpeed = 720;
 
     private void Awake()
     {
@@ -18,20 +19,21 @@ public class PlayerMovement : MonoBehaviour
             _touch = Input.GetTouch(0);
             if(_touch.phase == TouchPhase.Moved)
             {
-                Vector3 m_Input = new Vector3(
+                Vector3 _movementDirection = new Vector3(
                     transform.position.x + _touch.deltaPosition.x,
                     transform.position.y, 
                     transform.position.z + _touch.deltaPosition.y);
-                _rigidbody.MovePosition(transform.position + m_Input * Time.fixedDeltaTime * _speed);
+                
+                // Rigidbody.MovePosition() takes position in world space as parameter, not direction.
+                // World space position is basically what transform.position is.
+                Vector3 smoothedDelta = Vector3.MoveTowards(transform.position, _movementDirection, Time.fixedDeltaTime * _speed);
+                _rigidbody.MovePosition(smoothedDelta);
 
-                //Apply the movement vector to the current position, which is
-                //multiplied by deltaTime and speed for a smooth MovePosition
-                //_rigidbody.velocity = transform.position + m_Input;
-
-                //transform.position = new Vector3(
-                //    transform.position.x + _touch.deltaPosition.x * Time.fixedDeltaTime * _speed,
-                //    transform.position.y,
-                //    transform.position.z + _touch.deltaPosition.y * Time.fixedDeltaTime * _speed);
+                Vector3 newDirection = Vector3.RotateTowards(transform.forward, _movementDirection, _rotationSpeed * Time.fixedDeltaTime, 0f);
+                transform.rotation = Quaternion.LookRotation(newDirection);
+                
+                //Quaternion toRotation = Quaternion.LookRotation(_movementDirection, Vector3.up);
+                //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.fixedDeltaTime);
             }
         }
     }
