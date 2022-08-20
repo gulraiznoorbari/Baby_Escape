a6396f0dbfe934680945f1f0f636cbd9bfa8fe2a
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-21 01:56:57 +0500

    👌 IMPROVE: Refactor + Remove Unnecessary code

--- Game/Team_Mavericks-Baby_Escape/Assets/Scripts/EnemyMovement.cs
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,12 @@
 ﻿using UnityEngine;
 using DG.Tweening;
-using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
-    //[SerializeField] private Transform StartPoint;
-    //[SerializeField] private Transform EndPoint;
-    //[SerializeField] private float LerpSpeed = 1f;
-    //[SerializeField] private float _rotationSpeed = 40f;
     private Sequence _sequence;
 
     // Movement Using Tweening:
-
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -24,44 +18,11 @@ public class EnemyMovement : MonoBehaviour
         _sequence?.Kill();
 
         _sequence = DOTween.Sequence()
-            .Append(transform.DOMoveX(3, 1f))
-            .Append(transform.DORotate(new Vector3(0, 180, 0), 0.5f, RotateMode.FastBeyond360))
-            .Append(transform.DOMoveX(-3.25f, 1f))
-            .Append(transform.DORotate(new Vector3(0, 0, 0), 0.5f, RotateMode.FastBeyond360))
+            .Append(transform.DOMoveX(3, 1.5f))
+            .Append(transform.DORotate(new Vector3(0, 180, 0), 0.7f, RotateMode.FastBeyond360))
+            .Append(transform.DOMoveX(-3.3f, 1.5f))
+            .Append(transform.DORotate(new Vector3(0, 0, 0), 0.7f, RotateMode.FastBeyond360))
             .SetEase(Ease.Linear)
             .SetLoops(-1);
     }
-
-    // Movement Using Lerp() and Coroutine:
-
-    //private void Start()
-    //{
-    //    StartCoroutine(LerpRectFixedTime());
-    //}
-
-    //private IEnumerator LerpRectFixedTime()
-    //{
-    //    _rigidbody.transform.rotation = Quaternion.identity;
-
-    //    while (true)
-    //    {
-    //        float time = 0;
-    //        while (time < 1)
-    //        {
-    //            _rigidbody.transform.position = Vector3.Lerp(StartPoint.position, EndPoint.position, time);
-    //            transform.rotation = Quaternion.Slerp(transform.rotation, StartPoint.rotation, _rotationSpeed * Time.fixedDeltaTime * 360);
-    //            time += Time.deltaTime * LerpSpeed;
-    //            yield return null;
-    //        }
-
-    //        time = 0;
-    //        while (time < 1)
-    //        {
-    //            _rigidbody.transform.position = Vector3.Lerp(EndPoint.position, StartPoint.position, time);
-    //            transform.rotation = Quaternion.Slerp(transform.rotation, EndPoint.rotation, _rotationSpeed * Time.fixedDeltaTime * 360);
-    //            time += Time.deltaTime * LerpSpeed;
-    //            yield return null;
-    //        }
-    //    }
-    //}
 }
--- Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Touch _touch;
-    private float _speed = 5f;
-    private float _rotationSpeed = 720;
+    [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         if(Input.touchCount > 0)
         {
@@ -22,17 +22,24 @@ public class PlayerMovement : MonoBehaviour
                 Vector3 _movementDirection = new Vector3(
                     transform.position.x + _touch.deltaPosition.x,
                     transform.position.y, 
-                    transform.position.z + _touch.deltaPosition.y).normalized;
-                
+                    transform.position.z + _touch.deltaPosition.y);
+
                 // Rigidbody.MovePosition() takes position in world space as parameter, not direction.
                 // World space position is basically what transform.position is.
                 _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, _movementDirection, Time.fixedDeltaTime * _speed));
 
-                Quaternion toRotation = Quaternion.LookRotation(_movementDirection);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.fixedDeltaTime);
+                ////Rotate towards the direction of the movement (w/o slerp):
+                //Quaternion toRotation = Quaternion.LookRotation(_movementDirection);
+                //transform.rotation = Quaternion.RotateTowards(
+                //    transform.rotation, 
+                //    toRotation, 
+                //    _rotationSpeed * Time.fixedDeltaTime);
 
-                //rotate towards the direction of the movement
-                //transform.rotation = Quaternion.LookRotation(_movementDirection);
+                //Rotate towards the direction of the movement (w/ slerp):
+                _rigidbody.transform.rotation = Quaternion.Slerp(
+                    _rigidbody.transform.rotation,
+                    Quaternion.LookRotation(_movementDirection),
+                    Time.fixedDeltaTime * _rotationSpeed);
             }
         }
     }
