0c6a5eeb47c1554217581eadb8d8ff163739dbab
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-20 16:50:39 +0500

    📦 NEW: Add Enemy + Enemy Movement

---
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    //[SerializeField] private float Step = 2;
+    //[SerializeField] private Vector3 Direction = new Vector3(5,0,0);
+    //[SerializeField] private float MovementSpeed;
+    //[SerializeField] private float RaycastDistance = 2f;
+    //[SerializeField] private LayerMask LayerMask;
+    [SerializeField] private Transform StartPoint;
+    [SerializeField] private Transform EndPoint;
+    [SerializeField] private float LerpSpeed = 1f;
+    [SerializeField] private float _rotationSpeed = 40f;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        //Vector3 _currentAngles = transform.rotation.eulerAngles;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LerpRectFixedTime());
+    }
+
+    private IEnumerator LerpRectFixedTime()
+    {
+        _rigidbody.transform.rotation = Quaternion.identity;
+
+        while (true)
+        {
+            float time = 0;
+            while (time < 1)
+            {
+                _rigidbody.transform.position = Vector3.Lerp(StartPoint.position, EndPoint.position, time);
+                transform.rotation = Quaternion.Slerp(transform.rotation, StartPoint.rotation, _rotationSpeed * Time.fixedDeltaTime);
+                time += Time.deltaTime * LerpSpeed;
+                yield return null;
+            }
+
+            time = 0;
+            while (time < 1)
+            {
+                _rigidbody.transform.position = Vector3.Lerp(EndPoint.position, StartPoint.position, time);
+                transform.rotation = Quaternion.Slerp(transform.rotation, EndPoint.rotation, _rotationSpeed * Time.fixedDeltaTime);
+                time += Time.deltaTime * LerpSpeed;
+                yield return null;
+            }
+        }
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    var step = Step * Time.fixedDeltaTime;
+    //    var nextposition = transform.position + (Direction * step);
+    //    nextposition = Vector3.Lerp(transform.position, nextposition, MovementSpeed * Time.fixedDeltaTime);
+    //    _rigidbody.MovePosition(nextposition);
+
+    //    //DetectObjectThroughRaycast();
+    //}
+
+    //private void DetectObjectThroughRaycast()
+    //{
+    //    RaycastHit hit;
+    //    if (Physics.Raycast(transform.position, Direction, out hit, RaycastDistance, LayerMask))
+    //    {
+    //        if (hit.collider.CompareTag("Wall"))
+    //        {
+    //            RotateObject();
+    //            StopMovement();
+    //        }
+    //    }
+    //}
+
+    //private void StopMovement()
+    //{
+    //    _rigidbody.velocity = Vector3.zero;
+    //}
+
+    //private void RotateObject()
+    //{
+    //    Debug.Log(transform.rotation.eulerAngles);
+
+    //    Vector3 to = new Vector3(0, 180, 0);
+    //    Vector3 v3current = Vector3.Lerp(_currentAngles, to, _rotationSpeed * Time.fixedDeltaTime);
+    //    transform.eulerAngles = v3current;
+
+    //    transform.rotation = Quaternion.Slerp(transform.rotation, otherObject.rotation, _rotationSpeed * Time.fixedDeltaTime);
+
+    //    Debug.Log("Object Rotated!");
+    //    Debug.Log(transform.rotation.eulerAngles);
+    //}
+}
--- Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Touch _touch;
-    private float _speed = 10f;
+    private float _speed = 5f;
     private float _rotationSpeed = 720;
 
     private void Awake()
@@ -17,23 +17,22 @@ public class PlayerMovement : MonoBehaviour
         if(Input.touchCount > 0)
         {
             _touch = Input.GetTouch(0);
-            if(_touch.phase == TouchPhase.Moved)
+            if(_touch.phase == TouchPhase.Stationary || _touch.phase == TouchPhase.Moved)
             {
                 Vector3 _movementDirection = new Vector3(
                     transform.position.x + _touch.deltaPosition.x,
                     transform.position.y, 
-                    transform.position.z + _touch.deltaPosition.y);
+                    transform.position.z + _touch.deltaPosition.y).normalized;
                 
                 // Rigidbody.MovePosition() takes position in world space as parameter, not direction.
                 // World space position is basically what transform.position is.
-                Vector3 smoothedDelta = Vector3.MoveTowards(transform.position, _movementDirection, Time.fixedDeltaTime * _speed);
-                _rigidbody.MovePosition(smoothedDelta);
+                _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, _movementDirection, Time.fixedDeltaTime * _speed));
 
-                Vector3 newDirection = Vector3.RotateTowards(transform.forward, _movementDirection, _rotationSpeed * Time.fixedDeltaTime, 0f);
-                transform.rotation = Quaternion.LookRotation(newDirection);
-                
-                //Quaternion toRotation = Quaternion.LookRotation(_movementDirection, Vector3.up);
-                //transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.fixedDeltaTime);
+                Quaternion toRotation = Quaternion.LookRotation(_movementDirection);
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, _rotationSpeed * Time.fixedDeltaTime);
+
+                //rotate towards the direction of the movement
+                //transform.rotation = Quaternion.LookRotation(_movementDirection);
             }
         }
     }
