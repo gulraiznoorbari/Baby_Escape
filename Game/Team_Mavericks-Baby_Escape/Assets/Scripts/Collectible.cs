e9092ef18a1ac694367c19c6fb56cd214c46f310
Gulraiz <gulraiznoorbari@gmail.com>
2022-08-21 15:36:19 +0500

    📦 NEW: Add Collectible + Functionality

--- Game/Team_Mavericks-Baby_Escape/Assets/Scripts/Collectible.cs
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/Collectible.cs
@@ -1,9 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collectible : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        
+        transform.Rotate(0, 80 * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
