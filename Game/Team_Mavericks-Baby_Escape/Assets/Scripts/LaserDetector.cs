cde2e1e28407f5a583b2b6ecf1a29beeaacbae06
Gulraiz <gulraiznoorbari@gmail.com>
2022-09-08 00:44:43 +0500

    👌 IMPROVE: Line Rendering & Movement Detection

---
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/LaserDetector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class LaserDetector : MonoBehaviour
+{
+    [SerializeField] private LayerMask _layerMask;
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint; 
+
+    private LineRenderer _laserline;
+    private ParticleSystem _particleSystem;
+
+    private void Awake()
+    {
+        _laserline = GetComponent<LineRenderer>();
+        _particleSystem = GameObject.Find("startPoint").GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        _particleSystem.Stop();
+    }
+
+    public void InitiateLaser()
+    {
+        _laserline.startWidth = 0.2f;
+        _laserline.endWidth = 0.2f;
+        _laserline.SetPosition(0, _startPoint.position);
+        _laserline.SetPosition(1, _endPoint.position);
+        _particleSystem.Play();
+
+        DetectMovingObjects();
+    }
+
+    private void DetectMovingObjects()
+    {
+        RaycastHit hit;
+        float RaycastDistance = Vector3.Distance(_startPoint.position, _endPoint.position);
+        Vector3 direction = _endPoint.transform.position - _startPoint.transform.position;
+        // or you can simply use "transform.forward" for projecting in forward direction.
+
+        if (Physics.Raycast(transform.position, direction, out hit, RaycastDistance, _layerMask))
+        {
+            if (hit.collider.CompareTag("Player") || hit.collider.CompareTag("Enemy"))
+            {
+                Handheld.Vibrate();
+                Destroy(hit.collider.gameObject);
+            }
+        }
+    }
+}
