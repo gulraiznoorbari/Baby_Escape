5c121430d22b8e206356b5562813aa8eaade2bdd
Gulraiz <gulraiznoorbari@gmail.com>
2022-09-08 00:46:31 +0500

    📦 NEW: Add Laser Activation Script, Button & Animation

---
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/ActivateLaser.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ActivateLaser : MonoBehaviour
+{
+    [SerializeField] private LaserDetector _laserDetector;
+
+    private Animator _animator;
+
+    int PressedKey;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        int PressedKey = Animator.StringToHash("press");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Enemy"))
+        {
+            _animator.SetTrigger(PressedKey);
+            _laserDetector.InitiateLaser();
+            Debug.Log("Button Pressed and Laser Activated!");
+        }
+    }
+}
