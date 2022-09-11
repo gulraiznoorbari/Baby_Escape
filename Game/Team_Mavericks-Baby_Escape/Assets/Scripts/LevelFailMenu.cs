29cab4091693942bf864b503af23c31f49712a17
Gulraiz <gulraiznoorbari@gmail.com>
2022-09-12 00:05:08 +0500

    📦 NEW: Add Level Completion & Failure Menu

---
+++ Game/Team_Mavericks-Baby_Escape/Assets/Scripts/LevelFailMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFailMenu : MonoBehaviour
+{
+    public void CurrentLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
